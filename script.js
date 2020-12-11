b399e64d6239cf39cb1fe02661e7360a77d6ba76
Isaac Batista <isaacbatst@gmail.com>
2020-12-11 15:10:58 -0300

    test eslint and stylint fails with editor config

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
-function addTask(){
-  let taskList= document.querySelector('#lista-tarefas');
-  let taskButton=document.querySelector('#criar-tarefa');
+function addTask() {
+  const taskList = document.querySelector('#lista-tarefas');
+  const taskButton = document.querySelector("#criar-tarefa");
 
-  taskButton.addEventListener('click',function(){
-      let inputText=document.querySelector('#texto-tarefa');
-      let taskElement=document.createElement('li');
-      taskElement.innerHTML=inputText.value;
-      taskList.appendChild(taskElement);
-      taskList.next;
-      inputText.value='';
-  })
+  taskButton.addEventListener('click', () => {
+      const inputText = document.querySelector('#texto-tarefa');
+    const taskElement = document.createElement('li');
+    taskElement.innerHTML = inputText.value;
+    taskList.appendChild(taskElement);
+    taskList.next;
+    inputText.value = '';
+  });
 }
 
 addTask();
 
 function addTaskColor() {
-  let taskList = document.querySelector("#lista-tarefas");
-  taskList.addEventListener('click', function(event) {
-  let chosen = document.querySelector('.chosen');
-  if (chosen == null) {
+  const taskList = document.querySelector('#lista-tarefas');
+  taskList.addEventListener('click', (event) => {
+    const chosen = document.querySelector('.chosen');
+    if (chosen == null) {
       event.target.classList.add('chosen');
-      event.target.style.backgroundColor = 'rgb(128, 128, 128)'
-  } else {
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    } else {
       chosen.classList.remove('chosen');
       chosen.style.backgroundColor = '';
       event.target.classList.add('chosen');
-      event.target.style.backgroundColor = 'rgb(128, 128, 128)'
-  }
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
   });
 }
 addTaskColor();
 
 function completedTasks() {
-  let item = document.querySelector('#lista-tarefas');
-  item.addEventListener('dblclick', function(event) {
-      if (event.target.classList.contains('completed')) {
-          event.target.classList.remove('completed');
-      } else {
-          event.target.classList.add('completed');
-      }
+  const item = document.querySelector('#lista-tarefas');
+  item.addEventListener('dblclick', (event) => {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
   });
 }
 completedTasks();
 
-function cleanList(){
-  let cleanButton= document.querySelector('#apaga-tudo');
-  cleanButton.addEventListener('click',function(){
-      let taskList=document.querySelector('#lista-tarefas');
-      taskList.innerHTML='';
-  })
+function cleanList() {
+  const cleanButton = document.querySelector('#apaga-tudo');
+  cleanButton.addEventListener('click', () => {
+    const taskList = document.querySelector('#lista-tarefas');
+    taskList.innerHTML = '';
+  });
 }
 cleanList();
 
 function eraseCompleted() {
-  let buttonEraseCompleted = document.querySelector('#remover-finalizados');
-  buttonEraseCompleted.addEventListener('click', function() {
-      let taskList = document.querySelector('#lista-tarefas');
-      for (let index = 0; index < taskList.children.length; index++) {
-          if (taskList.children[index].classList.contains('completed')) {
-              taskList.removeChild(taskList.children[index]);
-              index -= 1;
-          }
+  const buttonEraseCompleted = document.querySelector('#remover-finalizados');
+  buttonEraseCompleted.addEventListener('click', () => {
+    const taskList = document.querySelector('#lista-tarefas');
+    for (let index = 0; index < taskList.children.length; index++) {
+      if (taskList.children[index].classList.contains('completed')) {
+        taskList.removeChild(taskList.children[index]);
+        index -= 1;
       }
+    }
   });
 }
 eraseCompleted();
